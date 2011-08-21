fde49b211fa3269a68714d6ff7acc59e2c6636ea
Jason Morrissey <me@jasonmorrissey.org>
2011-08-21 13:12:24 +1000

    Made the tabview thinner for iPhone displays

--- JMTabView/Classes/Subviews/JMTabConstants.h
+++ JMTabView/Classes/Subviews/JMTabConstants.h
@@ -2,7 +2,7 @@
 
 #define kTabItemFont [UIFont boldSystemFontOfSize:12.]
 #define kTabItemTextColor [UIColor whiteColor]
-#define kTabItemPadding CGSizeMake(16., 10.)
+#define kTabItemPadding CGSizeMake(16., 8.)
 #define kTabItemIconMargin 8.
 #define kTabSpacing 14.
 #define kTabSelectionAnimationDuration 0.3
