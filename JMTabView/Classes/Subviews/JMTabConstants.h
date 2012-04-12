2f696291c692f4a27e73374416d28e202318ace2
William Towe <willtowe@maestroelearning.com>
2012-04-12 15:36:01 -0400

    adjusted appearance of JMTabView
    tabs are closer together, adjusted the height to match UINavigationBar, changed the height of BarBackgroundLayer to match new height

--- JMTabView/Classes/Subviews/JMTabConstants.h
+++ JMTabView/Classes/Subviews/JMTabConstants.h
@@ -2,8 +2,8 @@
 
 #define kTabItemFont [UIFont boldSystemFontOfSize:12.]
 #define kTabItemTextColor [UIColor whiteColor]
-#define kTabItemPadding CGSizeMake(16., 10.)
+#define kTabItemPadding CGSizeMake(5., 5.)
 #define kTabItemIconMargin 8.
-#define kTabSpacing 14.
+#define kTabSpacing 10.
 #define kTabSelectionAnimationDuration 0.3
 
