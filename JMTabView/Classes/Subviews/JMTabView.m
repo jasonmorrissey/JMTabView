bb99afcbd2f69f1a991d53f5a1a1774aa6b58ef6
Jason Morrissey <me@jasonmorrissey.org>
2012-04-13 11:50:39 +1000

    Fixed incorrect respondsToSelector check

--- JMTabView/Classes/Subviews/JMTabView.m
+++ JMTabView/Classes/Subviews/JMTabView.m
@@ -73,7 +73,7 @@
 
 - (void)didSelectItemAtIndex:(NSUInteger)itemIndex;
 {
-    if (self.delegate && [self.delegate respondsToSelector:@selector(didSelectTabAtIndex:)])
+    if (self.delegate && [self.delegate respondsToSelector:@selector(tabView:didSelectTabAtIndex:)])
     {
         [self.delegate tabView:self didSelectTabAtIndex:itemIndex];
     }
